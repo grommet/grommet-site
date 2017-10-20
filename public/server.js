4570e064b4f2fc6571ecd4c42697934fa27e01f3
Alan Souza <alansouzati@gmail.com>
2017-10-20 15:05:46 -0700

    Added a simple node server

---
+++ public/server.js
@@ -0,0 +1,27 @@
+// (C) Copyright 2014-2016 Hewlett Packard Enterprise Development Company, L.P.
+
+/* eslint-disable import/no-unresolved */
+const compression = require('compression');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+/* eslint-enable import/no-unresolved */
+
+const PORT = process.env.PORT || 8050;
+
+const app = express();
+
+app.use(compression());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.sendFile(path.resolve(path.join(__dirname, './index.html'))));
+
+app.use('/', express.static(path.resolve(__dirname)));
+
+const server = http.createServer(app);
+server.listen(PORT);
+
+console.log(`Server started, listening at: http://localhost:${PORT}...`);
