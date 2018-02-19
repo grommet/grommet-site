f401483f02a0de7b15d51b2eeacdd27fdb296fad
Eric Soderberg <eric.soderberg@hpe.com>
2018-02-18 22:45:38 -0800

    Filled out a bit more to Feed

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Heading, Paragraph } from 'grommet';
+import { Box, Button, Heading, Paragraph } from 'grommet';
+import { Twitter, Vimeo } from 'grommet-icons';
 import Nav from '../components/Nav';
 
 export default class Feed extends Component {
@@ -20,13 +21,36 @@ export default class Feed extends Component {
                 <strong>More stuff to read, see, and hear</strong>
               </Heading>
               <Paragraph size='large'>
-                These are the building blocks of the grommet libray, master them, and become a l33t.
-              </Paragraph>
-              <Paragraph size='large'>
-                <strong>TBD</strong>
+                A growing list of resources for grommet design and development,
+                contributed by you.
               </Paragraph>
             </Box>
           </Box>
+        </Box>
+
+        <Box
+          direction='row'
+          wrap={true}
+          pad={{ horizontal: 'large', bottom: 'large' }}
+          gap='large'
+        >
+          <Button href='https://vimeo.com/grommetux'>
+            <Box pad='medium' round='medium' background='neutral-1'>
+              <Vimeo size='large' />
+              <Paragraph>
+                Learn more about grommet from our screencasts.
+              </Paragraph>
+            </Box>
+          </Button>
+
+          <Button href='https://twitter.com/grommetux'>
+            <Box pad='medium' round='medium' background='neutral-2'>
+              <Twitter size='large' />
+              <Paragraph>
+                Keep up with the latest grommet news.
+              </Paragraph>
+            </Box>
+          </Button>
         </Box>
       </Box>
     );
