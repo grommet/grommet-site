15597f6bdf4a834d097c3beb9cc89c7c71cbc22b
Alan Souza <alansouzati@gmail.com>
2018-08-31 11:06:57 -0700

    disabled optimization plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -14,27 +15,28 @@ module.exports = {
     port: 8567,
   },
   entry: './src/index.js',
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        grommet: {
-          test: /[\\/]node_modules[\\/]grommet/,
-          name: 'grommet',
-          priority: -10,
-        },
-        vendors: {
-          test: context => (
-            context.resource
-            && context.resource.search(/[\\/]node_modules[\\/]/) !== -1
-            && context.resource.search(/[\\/]node_modules[\\/]grommet/) === -1
-          ),
-          name: 'vendors',
-          priority: -10,
-        },
-      },
-    },
-  },
+  // does not work with monaco editor.
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all',
+  //     cacheGroups: {
+  //       grommet: {
+  //         test: /[\\/]node_modules[\\/]grommet/,
+  //         name: 'grommet',
+  //         priority: -10,
+  //       },
+  //       vendors: {
+  //         test: context => (
+  //           context.resource
+  //           && context.resource.search(/[\\/]node_modules[\\/]/) !== -1
+  //           && context.resource.search(/[\\/]node_modules[\\/]grommet/) === -1
+  //         ),
+  //         name: 'vendors',
+  //         priority: -10,
+  //       },
+  //     },
+  //   },
+  // },
   output: {
     path: path.resolve('./dist'),
     filename: '[name]-[hash].js',
