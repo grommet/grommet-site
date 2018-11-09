0cbdacf78febefa29a49ba8c7e618eb484c6b4ea
Alan Souza <alansouzati@gmail.com>
2018-11-08 17:20:19 -0800

    Added heroku back

--- tools/release-heroku.js
+++ tools/release-heroku.js
@@ -3,27 +3,25 @@ import git from 'simple-git/promise';
 import del from 'del';
 import fs from 'fs-extra';
 
-const repoURL = `https://${
-  process.env.GH_TOKEN
-}@github.com/grommet/grommet.github.io.git`;
+const repoURL = `https://github.com/grommet/grommet-site.git`;
 const localFolder = path.resolve('./.tmp/grommet-site');
 const localDist = path.resolve('./dist');
 
-if (process.env.CI) {
+if (true || process.env.CI) {
   del(localFolder).then(() => {
     git()
       .silent(true)
       .clone(repoURL, localFolder)
-      .then(() => git(localFolder).checkout('master'))
+      .then(() => git(localFolder).checkout('heroku'))
       .then(() => del([`${localFolder}/**/*`]))
       .then(() => fs.copy(localDist, localFolder))
       .then(() => git(localFolder).add(['--all', '.']))
-      .then(() => git(localFolder).commit('gh-pages updated'))
-      .then(() => git(localFolder).push('origin', 'master'))
+      .then(() => git(localFolder).commit('heroku updated'))
+      .then(() => git(localFolder).push('origin', 'heroku'))
       .catch(err => console.error('failed: ', err));
   });
 } else {
   console.warn(
-    'Skipping release. Release:gh-pages task should be executed by CI only.',
+    'Skipping release. Release:heroku task should be executed by CI only.',
   );
 }
