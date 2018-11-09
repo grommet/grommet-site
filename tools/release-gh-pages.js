9c3595d05d80c699049e61b600e58b107849e896
Alan Souza <alansouzati@gmail.com>
2018-11-08 17:10:47 -0800

    Added travis update to the new site.

--- tools/release-gh-pages.js
+++ tools/release-gh-pages.js
@@ -3,22 +3,27 @@ import git from 'simple-git/promise';
 import del from 'del';
 import fs from 'fs-extra';
 
-const repoURL = `https://${process.env.GH_TOKEN}@github.com/grommet/grommet-site.git`;
+const repoURL = `https://${
+  process.env.GH_TOKEN
+}@github.com/grommet/grommet.github.io.git`;
 const localFolder = path.resolve('./.tmp/grommet-site');
 const localDist = path.resolve('./dist');
 
 if (process.env.CI) {
   del(localFolder).then(() => {
-    git().silent(true)
+    git()
+      .silent(true)
       .clone(repoURL, localFolder)
-      .then(() => git(localFolder).checkout('heroku'))
+      .then(() => git(localFolder).checkout('master'))
       .then(() => del([`${localFolder}/**/*`]))
       .then(() => fs.copy(localDist, localFolder))
       .then(() => git(localFolder).add(['--all', '.']))
-      .then(() => git(localFolder).commit('heroku updated'))
-      .then(() => git(localFolder).push('origin', 'heroku'))
+      .then(() => git(localFolder).commit('gh-pages updated'))
+      .then(() => git(localFolder).push('origin', 'master'))
       .catch(err => console.error('failed: ', err));
   });
 } else {
-  console.warn('Skipping release. Release:heroku task should be executed by CI only.');
+  console.warn(
+    'Skipping release. Release:gh-pages task should be executed by CI only.',
+  );
 }
