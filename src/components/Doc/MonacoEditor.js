876bc683758a1881ad2631a1f665d72820178461
Alex Mejias <contact@lazers.tv>
2019-11-19 14:14:19 -0500

    Fix Monaco Editor conditional import.

--- src/components/Doc/MonacoEditor.js
+++ src/components/Doc/MonacoEditor.js
@@ -3,7 +3,11 @@ import React from 'react';
 // React Monaco Editor has a SSR Babel bug
 // link: https://github.com/react-monaco-editor/react-monaco-editor/issues/133
 const isWindow = typeof window !== 'undefined';
-const ReactMonacoEditor = isWindow ? 'react-monaco-editor' : undefined;
+const ReactMonacoEditor = isWindow
+  ? require('react-monaco-editor').default
+  : undefined;
 
-export const MonacoEditor = props =>
-  isWindow ? <ReactMonacoEditor {...props} /> : undefined; // eslint-disable-line react/jsx-props-no-spreading
+const MonacoEditor = props =>
+  ReactMonacoEditor ? <ReactMonacoEditor {...props} /> : undefined; // eslint-disable-line react/jsx-props-no-spreading
+
+export default MonacoEditor;
