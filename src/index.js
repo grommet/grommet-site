433d1f7f0dfc84bb1131e62af48cefd3751dc863
Alex Mejias <contact@lazers.tv>
2019-11-19 13:00:10 -0500

    Fix PWA during SSR.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
 // the following polyfills are included for IE11 compatibility
 import 'core-js/features/string/starts-with'; // used by react-router
@@ -8,7 +7,10 @@ import 'core-js/features/string/ends-with'; // used by components/Doc/ThemeProps
 
 import App from './App';
 
-OfflinePluginRuntime.install();
+if (typeof window !== 'undefined') {
+  const OfflinePluginRuntime = require('offline-plugin/runtime'); // eslint-disable-line global-require
+  OfflinePluginRuntime.install();
+}
 
 const element = document.getElementById('content');
 ReactDOM.render(<App />, element);
