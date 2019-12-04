5a5dbc8819c9cb396971345c455d156ecf1fc5a4
shimisun <shimrit.yacobi@gmail.com>
2019-12-04 15:02:53 -0800

    Changed Header code example

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Header, Box } from 'grommet';
+import { Gremlin } from 'grommet-icons';
 import { doc } from 'grommet/components/Header/doc';
 
 import Page from '../components/Page';
@@ -14,16 +15,25 @@ export default () => (
     <Doc
       name="Header"
       desc={desc}
-      code={`<Header background="red" pad="large"><Box>Hi</Box><Box>Shimi</Box></Header>`}
+      code={`<Header background="light-4" pad="small" gap="large">
+  <Box direction="row" gap="xsmall">
+    <Icons.Gremlin color="brand"/>
+    <Box>Header</Box>
+  </Box>
+  <Box>Nav-options</Box>
+</Header>`}
     />
   </Page>
 );
 
 export const HeaderItem = ({ name, path }) => (
   <Item name={name} path={path} center>
-    <Header background="red" pad="large">
-      <Box>Hi</Box>
-      <Box>Shimi</Box>
+    <Header background="brand" pad="small" gap="large">
+      <Box direction="row" gap="xsmall">
+        <Gremlin />
+        <Box>Header</Box>
+      </Box>
+      <Box>Nav-options</Box>
     </Header>
   </Item>
 );
