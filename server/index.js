059a42ae554db38099915d8097539d5a12308bf2
Alex Mejias <contact@lazers.tv>
2019-12-14 10:51:36 -0800

    Add static rendering comments.

--- server/index.js
+++ server/index.js
@@ -10,22 +10,19 @@ import html from './html';
 
 const publicPath = path.resolve(__dirname, '..', 'dist');
 
+// The production JS bundle must be created before
+// the static HTML files.
 if (!fs.existsSync(publicPath)) {
   throw new Error(
     `No distribution folder detected. Run 'yarn build' first to create the site bundle.`,
   );
 }
 
+// Grab Webpack's manifest to determine the latest JS bundle hash.
 const manifest = require(`${publicPath}/webpack-manifest.json`); // eslint-disable-line import/no-dynamic-require
-const linksToVisit = [];
-const linksVisited = [];
-let isDone = false;
 
-const hasVisited = link => {
-  const linkVisited = linksVisited.find(item => item === link);
-  return typeof linkVisited === 'string';
-};
-
+// Render a React output as string and dynamically inject
+// to an HTML string which a browser can consume.
 const renderHtmlString = pagePath => {
   const sheet = new ServerStyleSheet();
   const body = renderToString(
@@ -38,20 +35,38 @@ const renderHtmlString = pagePath => {
   return htmlString;
 };
 
+const linksToVisit = [];
+const linksVisited = [];
+// Our page crawling queue uses a simple boolean to it's done
+// TODO: refactor to more elegant queue.
+let isDone = false;
+
+// Check if the page crawler has already visited a page.
+const hasVisited = link => {
+  const linkVisited = linksVisited.find(item => item === link);
+  return typeof linkVisited === 'string';
+};
+
+// Crawl a path in our React app to discover app pages
 function crawlForLinks(pagePath, done) {
   linksVisited.push(pagePath);
   const renderHtml = renderHtmlString(pagePath);
+  // Initiate our page in cheerio, a light-weight jQuery-like library.
   const $ = cheerio.load(renderHtml);
+  // Use cheerio to find all of the links on the pagePath
   const links = $('a');
 
   $(links).each((i, link) => {
     const linkHref = $(link).attr('href');
+    // If the crawled link points to our app add it
+    // to the queue to be crawled.
     if (
       linkHref &&
       typeof linkHref === 'string' &&
       linkHref.charAt(0) === '/' &&
       linkHref.charAt(1) !== '/'
     ) {
+      // Avoid queue duplicates and performance bottlenecks.
       if (!hasVisited(linkHref)) {
         linksToVisit.push(linkHref);
       }
@@ -61,6 +76,7 @@ function crawlForLinks(pagePath, done) {
   linksToVisit.forEach(link => {
     if (!hasVisited(link)) {
       console.log('queueing', link);
+      // Use recursion to dig deeper into the app.
       crawlForLinks(link, done);
     }
   });
@@ -69,25 +85,32 @@ function crawlForLinks(pagePath, done) {
   }
 }
 
-crawlForLinks('/', () => {
+const generateHtmlPages = () => {
+  // TODO: Improve page link queueing
   isDone = true;
-  // dedupe just in case
+  // Dedupe the array just in case.
   const siteLinks = [...new Set(linksVisited)];
 
   siteLinks.forEach(page => {
     const fileName = `${page.replace('/', '')}.html`;
     const filePath = path.resolve(
-      __dirname,
-      '..',
-      'dist',
+      publicPath,
+      // handle the '.html' edge case for the index file.
       fileName !== '.html' ? fileName : 'index.html',
     );
+    // Create the new file
     const stream = fs.createWriteStream(filePath);
+    // TODO: improve perf by only rendering to string
+    // a single time.
     const pageHtml = renderHtmlString(page);
 
     console.log('Creating page: ', filePath);
     stream.once('open', () => {
+      // Add our HTML string to the file.
       stream.end(pageHtml);
     });
   });
-});
+};
+
+// Use our '/' path as the base to start crawling from.
+crawlForLinks('/', generateHtmlPages);
