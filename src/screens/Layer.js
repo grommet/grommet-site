f1aa82120ae123d67f18e446b78e079588e9f825
Jessica Filben <jessica.filben@gmail.com>
2021-07-26 10:22:33 -0600

    removed commented code

--- src/screens/Layer.js
+++ src/screens/Layer.js
@@ -315,49 +315,6 @@ export default () => (
       </ThemeDoc>
     </ComponentDoc>
   </Page>
-  //   <Page>
-  //     <Doc
-  //       name="Layer"
-  //       desc={desc}
-  //       syntaxes={{
-  //         margin: [
-  //           'none',
-  //           'xsmall',
-  //           'small',
-  //           'medium',
-  //           'large',
-  //           'any CSS size',
-  //           {
-  //             vertical: '...',
-  //             horizontal: '...',
-  //             top: '...',
-  //             bottom: '...',
-  //             left: '...',
-  //             right: '...',
-  //           },
-  //         ],
-  //         onClickOutside: '() => {}',
-  //         onEsc: '() => {}',
-  //       }}
-  //       code={`function Example() {
-  //   const [show, setShow] = React.useState();
-  //   return (
-  //     <Box>
-  //       <Button label="show" onClick={() => setShow(true)} />
-  //       {show && (
-  //         <Layer
-  //           onEsc={() => setShow(false)}
-  //           onClickOutside={() => setShow(false)}
-  //         >
-  //           <Button label="close" onClick={() => setShow(false)} />
-  //         </Layer>
-  //       )}
-  //     </Box>
-  //   );
-  // }`}
-  //       themeDoc={themeDoc}
-  //     />
-  //   </Page>
 );
 
 export const LayerItem = ({ name, path }) => (
