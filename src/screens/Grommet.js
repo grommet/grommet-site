288c44eec81bdbb05731ccfba94d87f7d34f9921
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2021-07-26 11:23:00 -0600

    Updated Grommet doc to new format (#253)

--- src/screens/Grommet.js
+++ src/screens/Grommet.js
@@ -1,27 +1,292 @@
 import React from 'react';
-
-import { Grommet } from 'grommet';
 import { Grommet as GrommetIcon } from 'grommet-icons';
-import { doc, themeDoc } from 'grommet/components/Grommet/doc';
-
 import Page from '../components/Page';
-import Doc from '../components/Doc';
 import Item from './Components/Item';
-
-const desc = doc(Grommet).toJSON();
+import { GenericBoolFalse } from '../utils/genericPropExamples';
+import { GenericExtend } from '../utils/genericThemeExamples';
+import {
+  ComponentDoc,
+  Properties,
+  Property,
+  PropertyValue,
+  Description,
+  Example,
+  ThemeDoc,
+  PropOptions,
+} from '../components/Doc';
 
 export default () => (
   <Page>
-    <Doc
+    <ComponentDoc
       name="Grommet"
-      desc={desc}
+      availableAt={[
+        {
+          url:
+            'https://storybook.grommet.io/?selectedKind=Utilities-Grommet&full=0&stories=1&panelRight=0',
+          badge:
+            'https://cdn-images-1.medium.com/fit/c/120/120/1*TD1P0HtIH9zF0UEH28zYtw.png',
+          label: 'Storybook',
+        },
+        {
+          url:
+            'https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=/grommet&module=%2Fsrc%2FGrommet.js',
+          badge: 'https://codesandbox.io/static/img/play-codesandbox.svg',
+          label: 'CodeSandbox',
+        },
+      ]}
+      description="The top level Grommet container"
+      intrinsicElement="div"
       code={`<Grommet
-  theme={{ global: { colors: { doc: '#ff99cc' } } }}
->
+  theme={{ global: { 
+      colors: { doc: '#ff99cc' } 
+      }
+    }}
+  >
   <Box pad="large" background="doc" />
 </Grommet>`}
-      themeDoc={themeDoc}
-    />
+    >
+      <Properties>
+        <Property name="background">
+          <Description>
+            Either a color identifier to use for the background color. For
+            example: 'neutral-1'. Or, a 'url()' for an image. Dark is not needed
+            if color is provided.
+          </Description>
+          <PropertyValue type="string">
+            <Description>A color or image url.</Description>
+            <Example>"neutral-1"</Example>
+            <Example>"url(//my.com/assets/img.png)"</Example>
+          </PropertyValue>
+          <PropertyValue type="object">
+            <Example>
+              {`
+{
+  color: "neutral-1",
+  dark: true,
+  opacity: true,
+  position: "bottom",
+  repeat: "no-repeat",
+  size: "cover",
+  image: "url(//my.com/assets/img.png)",
+  light: "string"
+}
+              `}
+            </Example>
+            <Example>{`{ dark: "light-2", light: "dark-2" }`}</Example>
+            <PropOptions prop="opacity">
+              <Example>"weak"</Example>
+              <Example>"medium"</Example>
+              <Example>"strong"</Example>
+              <Example>true</Example>
+            </PropOptions>
+            <PropOptions prop="position">
+              <Example>any CSS for background-position</Example>
+            </PropOptions>
+            <PropOptions prop="repeat">
+              <Example>"no-repeat"</Example>
+              <Example>"repeat"</Example>
+              <Example>"string"</Example>
+            </PropOptions>
+            <PropOptions prop="size">
+              <Example>"cover"</Example>
+              <Example>"contain"</Example>
+              <Example>"string"</Example>
+            </PropOptions>
+          </PropertyValue>
+        </Property>
+
+        <Property name="dir">
+          <Description>Layout direction for right to left contexts</Description>
+          <PropertyValue type="string">
+            <Example>"rtl"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="full">
+          <Description>Whether to take the whole viewport.</Description>
+          <GenericBoolFalse />
+          <PropertyValue type="string">
+            <Description>
+              'min' indicates it should take at minimum the whole viewport,
+              allowing the viewport to control scrolling.
+            </Description>
+            <Example>"min"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="options">
+          <Description>
+            Provides a way to assign a unique id to a single DOM node.
+            Currently, this is only supported for Layer. This prop was created
+            to preserve backwards compatibility with existing behavior by
+            allowing users to opt-in to newer behavior.
+          </Description>
+          <PropertyValue type="object">
+            <Example>
+              {`
+{
+  layer: 
+  {
+    singleId: boolean
+  }
+}
+            `}
+            </Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="plain">
+          <Description>
+            Whether or not Grommet should apply a global font-family, font-size,
+            and line-height.
+          </Description>
+          <GenericBoolFalse />
+        </Property>
+
+        <Property name="cssVars">
+          <Description>Whether to expose the css variables.</Description>
+          <GenericBoolFalse />
+        </Property>
+
+        <Property name="theme">
+          <Description>Custom styles for Grommet app component.</Description>
+          <PropertyValue type="object">
+            <Example>
+              {`
+{
+  global: {},
+  anchor: {},
+  ...
+}
+            `}
+            </Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="themeMode">
+          <Description>
+            Dark vs. light theme variation. Default is unspecified and left to
+            theme.
+          </Description>
+          <PropertyValue type="string">
+            <Example>"dark"</Example>
+            <Example>"light"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="userAgent">
+          <Description>
+            User agent used to detect the device width for setting the initial
+            breakpoint. Full list can be found at
+            [deviceatlas.com](https://deviceatlas.com/blog/list-of-user-agent-strings)
+          </Description>
+          <PropertyValue type="string">
+            <Example>
+              "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML,
+              like Gecko) Chrome/47.0.2526.111 Safari/537.36
+            </Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="containerTarget">
+          <Description>
+            The node where Drop and Layer containers are inserted.
+          </Description>
+          <PropertyValue defaultValue type="object">
+            <Description>
+              Defaults to document.body which is almost always the right choice.
+              This is used for less common cases like rendering within an
+              internal node (e.g. shadow root).
+            </Description>
+            <Example>document.body</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="messages">
+          <Description>
+            Custom messages for grommet components. Use this property to define
+            messages or a function to get localized messages for any grommet
+            children components.
+          </Description>
+          <PropertyValue type="object">
+            <Description>
+              'format' is a function that will that will be called with an
+              options object parameter containing the id of the message needed.
+              This function should return the text string for that message. If
+              this format function is not defined, messages will be used from
+              the messages object below or a default message if the id wasn't
+              defined in this object.
+            </Description>
+            <Example>
+              {`
+{
+  format: function,
+  messages: {
+    fileInput: {
+      browse: "string",
+      dropPrompt: "string",
+      dropPromptMultiple: "string",
+      files: "string",
+      remove: "string",
+      removeAll: "string"
+    },
+    form: {
+      invalid: "string",
+      required: "string"
+    },
+    menu: {
+      openMenu: "string",
+      closeMenu: "string"
+    },
+    rangeSelector: {
+      lower: "string",
+      upper: "string"
+    },
+    select: { multiple: "string" },
+    skipLinks: { skipTo: "string" },
+    tabs: { tabContents: "string" },
+    textInput: {
+      enterSelect: "string",
+      suggestionsCount: "string",
+      suggestionsExist: "string",
+      suggestionIsOpen: "string"
+    },
+    video: {
+      closeMenu: "string",
+      fullScreen: "string",
+      progressMeter: "string",
+      scrubber: "string",
+      openMenu: "string",
+      pauseButton: "string",
+      playButton: "string",
+      volumeDown: "string",
+      volumeUp: "string"
+    }
+  }
+}
+            `}
+            </Example>
+          </PropertyValue>
+        </Property>
+      </Properties>
+
+      <ThemeDoc>
+        <Property name="grommet.extend">
+          <Description>Any additional style for Grommet.</Description>
+          <GenericExtend />
+        </Property>
+
+        <Property name="global.font.face">
+          <Description>Custom font face declaration.</Description>
+          <PropertyValue type="string">
+            <Example>"font"</Example>
+          </PropertyValue>
+          <PropertyValue type="object">
+            <Example>{`(props) => {}`}</Example>
+          </PropertyValue>
+        </Property>
+      </ThemeDoc>
+    </ComponentDoc>
   </Page>
 );
 
