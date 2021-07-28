faa550187ce9c8bd2287255ccc44db8132b690fd
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2021-07-28 11:33:28 -0600

    Updated Sidebar doc to new format (#276)

--- src/screens/Sidebar.js
+++ src/screens/Sidebar.js
@@ -1,33 +1,50 @@
 import React from 'react';
-
 import { Sidebar } from 'grommet';
-import { doc } from 'grommet/components/Sidebar/doc';
-
 import Page from '../components/Page';
-import Doc from '../components/Doc';
 import Item from './Components/Item';
-
-const desc = doc(Sidebar).toJSON();
+import {
+  ComponentDoc,
+  Properties,
+  Property,
+  PropertyValue,
+  Description,
+  Example,
+} from '../components/Doc';
 
 export default () => (
   <Page>
-    <Doc
+    <ComponentDoc
       name="Sidebar"
-      desc={desc}
-      code={`    
-    <Sidebar background="brand" round="small"
-      header={
-        <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
-      }
-      footer={
-        <Button icon={<Icons.Help />} hoverIndicator />
-      }
-    >
-      <Nav gap="small">
-        <Button icon={<Icons.Projects />} hoverIndicator />
-        <Button icon={<Icons.Clock />} hoverIndicator />
-      </Nav>
-    </Sidebar>`}
+      availableAt={[
+        {
+          url:
+            'https://storybook.grommet.io/?selectedKind=Layout-Sidebar&full=0&stories=1&panelRight=0',
+          badge:
+            'https://cdn-images-1.medium.com/fit/c/120/120/1*TD1P0HtIH9zF0UEH28zYtw.png',
+          label: 'Storybook',
+        },
+        {
+          url:
+            'https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=/sidebar&module=%2Fsrc%2FSidebar.js',
+          badge: 'https://codesandbox.io/static/img/play-codesandbox.svg',
+          label: 'CodeSandbox',
+        },
+      ]}
+      description="A sidebar, typically used with Nav children"
+      intrinsicElement="div"
+      code={`<Sidebar background="brand" round="small"
+  header={
+    <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+  }
+  footer={
+    <Button icon={<Icons.Help />} hoverIndicator />
+  }
+>
+  <Nav gap="small">
+    <Button icon={<Icons.Projects />} hoverIndicator />
+    <Button icon={<Icons.Clock />} hoverIndicator />
+  </Nav>
+</Sidebar>`}
       isA={{
         base: 'Box',
         path: '/box',
@@ -37,7 +54,27 @@ export default () => (
           height: "min: '100%'",
         },
       }}
-    />
+    >
+      <Properties>
+        <Property name="footer">
+          <Description>
+            If specified, a footer element for the Sidebar.
+          </Description>
+          <PropertyValue type="node">
+            <Example>{`<SidebarFooter />`}</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="header">
+          <Description>
+            If specified, an header element for the Sidebar.
+          </Description>
+          <PropertyValue type="node">
+            <Example>{`<SidebarHeader />`}</Example>
+          </PropertyValue>
+        </Property>
+      </Properties>
+    </ComponentDoc>
   </Page>
 );
 
