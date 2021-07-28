7b4543686829e6b4dd3f9d3c201ff1249f02a899
Jessica Filben <jessica.filben@gmail.com>
2021-07-28 16:24:06 -0600

    Updated Tip component to new format

--- src/screens/Tip.js
+++ src/screens/Tip.js
@@ -1,28 +1,106 @@
 import React from 'react';
-
-import { Box, Tip } from 'grommet';
-import { doc, themeDoc } from 'grommet/components/Tip/doc';
-
+import { Box } from 'grommet';
 import Page from '../components/Page';
-import Doc from '../components/Doc';
 import Item from './Components/Item';
-
-const desc = doc(Tip).toJSON();
+import { GenericBool } from '../utils/genericPropExamples';
+import {
+  ComponentDoc,
+  Properties,
+  Property,
+  PropertyValue,
+  Description,
+  Example,
+  ThemeDoc,
+} from '../components/Doc';
 
 export default () => (
   <Page>
-    <Doc
+    <ComponentDoc
       name="Tip"
-      desc={desc}
+      availableAt={[
+        {
+          url:
+            'https://storybook.grommet.io/?selectedKind=Controls-Tip&full=0&stories=1&panelRight=0',
+          badge:
+            'https://cdn-images-1.medium.com/fit/c/120/120/1*TD1P0HtIH9zF0UEH28zYtw.png',
+          label: 'Storybook',
+        },
+        {
+          url:
+            'https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=/tip&module=%2Fsrc%2FTip.js',
+          badge: 'https://codesandbox.io/static/img/play-codesandbox.svg',
+          label: 'CodeSandbox',
+        },
+      ]}
+      description="Tooltip or a hint when hovering over an element"
       code={`<Grommet theme={grommet}>
   <Box pad="medium">
     <Tip content="tooltip">
-     <Button label="hover me" /> 
+      <Button label="hover me" />
     </Tip>
   </Box>
 </Grommet>`}
-      themeDoc={themeDoc}
-    />
+    >
+      <Properties>
+        <Property name="content">
+          <Description>The tooltip content inside the drop.</Description>
+          <PropertyValue type="node">
+            <Example>"Additional info"</Example>
+            <Example>{`<Box>...</Box>`}</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="dropProps">
+          <Description>
+            Any valid Drop prop to style the Tip drop container.
+          </Description>
+          <PropertyValue type="object">
+            <Example defaultValue>{`{ trapFocus: "false" }`}</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="plain">
+          <Description>
+            Whether content should have default styling from tip.content.
+          </Description>
+          <GenericBool />
+        </Property>
+      </Properties>
+
+      <ThemeDoc>
+        <Property name="tip.content">
+          <Description>
+            Any valid Box property for the Tip container. Not applicable when
+            using Tip plain prop.
+          </Description>
+          <PropertyValue type="object">
+            <Example defaultValue>
+              {`{ 
+  background: "background-contrast", 
+  elevation: "small", 
+  margin: "xsmall", 
+  pad: { vertical: "xsmall", horizontal: "small" }, 
+  round: "small"
+}`}
+            </Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="tip.drop">
+          <Description>Any valid Drop property for the Tip.</Description>
+          <PropertyValue type="object">
+            <Example defaultValue>
+              {`{
+  align: { top: "bottom" }, 
+  background: "none",
+  elevation: "none",
+  margin: "none"
+}`}
+            </Example>
+          </PropertyValue>
+        </Property>
+      </ThemeDoc>
+    </ComponentDoc>
   </Page>
 );
 
