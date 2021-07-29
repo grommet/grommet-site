29ff1f2f33ade78bd53c114c0fe19e9acc09f876
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2021-07-29 09:53:55 -0600

    Updated RangeInput doc to new format (#272)

--- src/screens/RangeInput.js
+++ src/screens/RangeInput.js
@@ -1,19 +1,45 @@
 import React from 'react';
-
 import { RangeInput } from 'grommet';
-import { doc, themeDoc } from 'grommet/components/RangeInput/doc';
-
 import Page from '../components/Page';
-import Doc from '../components/Doc';
 import Item from './Components/Item';
-
-const desc = doc(RangeInput).toJSON();
+import { GenericA11yTitle } from '../utils/genericPropExamples';
+import {
+  GenericExtend,
+  GenericOpacity,
+  GenericColor,
+} from '../utils/genericThemeExamples';
+import {
+  ComponentDoc,
+  Properties,
+  Property,
+  PropertyValue,
+  Description,
+  Example,
+  ThemeDoc,
+} from '../components/Doc';
+import { FocusStyle } from '../utils/themeDocUtils';
 
 export default () => (
   <Page>
-    <Doc
+    <ComponentDoc
       name="RangeInput"
-      desc={desc}
+      availableAt={[
+        {
+          url:
+            'https://storybook.grommet.io/?selectedKind=Input-RangeInput&full=0&stories=1&panelRight=0',
+          badge:
+            'https://cdn-images-1.medium.com/fit/c/120/120/1*TD1P0HtIH9zF0UEH28zYtw.png',
+          label: 'Storybook',
+        },
+        {
+          url:
+            'https://codesandbox.io/s/github/grommet/grommet-sandbox?initialpath=/rangeinput&module=%2Fsrc%2FRangeInput.js',
+          badge: 'https://codesandbox.io/static/img/play-codesandbox.svg',
+          label: 'CodeSandbox',
+        },
+      ]}
+      description="A slider control to input a value within a fixed range"
+      intrinsicElement="input"
       code={`function Example() {
   const [value, setValue] = React.useState(10);
   return (
@@ -23,8 +49,174 @@ export default () => (
     />
   );
 }`}
-      themeDoc={themeDoc}
-    />
+    >
+      <Properties>
+        <Property name="a11yTitle">
+          <Description>
+            Custom label to be used by screen readers. When provided, an
+            aria-label will be added to the element.
+          </Description>
+          <GenericA11yTitle />
+        </Property>
+
+        <Property name="id">
+          <Description>The id attribute of the range input.</Description>
+          <PropertyValue type="string">
+            <Example>"inputId"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="min">
+          <Description>The minimum value permitted.</Description>
+          <PropertyValue type="number">
+            <Example>1</Example>
+          </PropertyValue>
+          <PropertyValue type="string">
+            <Example>"1"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="max">
+          <Description>The maximum value permitted.</Description>
+          <PropertyValue type="number">
+            <Example>10</Example>
+          </PropertyValue>
+          <PropertyValue type="string">
+            <Example>"10"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="name">
+          <Description>The name attribute of the range input.</Description>
+          <PropertyValue type="string">
+            <Example>"name"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="onChange">
+          <Description>
+            Function that will be called when the user changes the value. It
+            will be passed an event object. The new input value will be
+            available via 'event.target.value'.
+          </Description>
+          <PropertyValue type="function">
+            <Example>{`(event) => {}`}</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="step">
+          <Description>The step interval between values.</Description>
+          <PropertyValue type="number">
+            <Example>"2"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="value">
+          <Description>The current value.</Description>
+          <PropertyValue type="number">
+            <Example>1</Example>
+          </PropertyValue>
+          <PropertyValue type="string">
+            <Example>"1"</Example>
+          </PropertyValue>
+        </Property>
+      </Properties>
+
+      <ThemeDoc>
+        <FocusStyle />
+
+        <Property name="global.colors.border">
+          <Description>The color used for rangeInput.track.color.</Description>
+          <PropertyValue type="string">
+            <Description>A hex, name, or rgb value.</Description>
+            <Example>"brand"</Example>
+          </PropertyValue>
+          <PropertyValue type="object">
+            <Description>
+              An object with a color for dark and light modes.
+            </Description>
+            <Example defaultValue>
+              {`{ dark: "rgba(255, 255, 255, 0.33)", light: "rgba(0, 0, 0, 0.33)" }`}
+            </Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="global.spacing">
+          <Description>
+            The height, width and border-radius of the range thumb.
+          </Description>
+          <PropertyValue type="string">
+            <Example defaultValue>"24px"</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="rangeInput.extend">
+          <Description>Any additional style for the RangeInput.</Description>
+          <GenericExtend />
+        </Property>
+
+        <Property name="rangeInput.thumb.color">
+          <Description>The color of the thumb.</Description>
+          <GenericColor />
+        </Property>
+
+        <Property name="rangeInput.thumb.extend">
+          <Description>Any additional style for the thumb.</Description>
+          <GenericExtend />
+        </Property>
+
+        <Property name="rangeInput.track.color">
+          <Description>The color of the track.</Description>
+          <PropertyValue type="string">
+            <Description>A hex, name, or rgb value.</Description>
+            <Example defaultValue>"border"</Example>
+          </PropertyValue>
+          <PropertyValue type="object">
+            <Description>
+              An object with a color for dark and light modes.
+            </Description>
+            <Example>{`{ dark: "string", light: "string" }`}</Example>
+          </PropertyValue>
+        </Property>
+
+        <Property name="rangeInput.track.opacity">
+          <Description>The opacity of the track color.</Description>
+          <GenericOpacity />
+        </Property>
+
+        <Property name="rangeInput.track.lower.color">
+          <Description>The color of the lower bound track.</Description>
+          <GenericColor />
+        </Property>
+
+        <Property name="rangeInput.track.lower.opacity">
+          <Description>The opacity on the lower bound track color.</Description>
+          <GenericOpacity />
+        </Property>
+
+        <Property name="rangeInput.track.upper.color">
+          <Description>The color of the upper track.</Description>
+          <GenericColor />
+        </Property>
+
+        <Property name="rangeInput.track.upper.opacity">
+          <Description>The opacity on the upper track color.</Description>
+          <GenericOpacity />
+        </Property>
+
+        <Property name="rangeInput.track.extend">
+          <Description>Any additional style for the track.</Description>
+          <GenericExtend />
+        </Property>
+
+        <Property name="rangeInput.track.height">
+          <Description>The height of the track.</Description>
+          <PropertyValue type="string">
+            <Example defaultValue>"4px"</Example>
+          </PropertyValue>
+        </Property>
+      </ThemeDoc>
+    </ComponentDoc>
   </Page>
 );
 
