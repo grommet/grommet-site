ea7e6b505b6f9866bc3c1b12961d0678ad10f602
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2021-08-16 15:06:56 -0600

    Fix prop anchor links to scroll to location (#296)
    * fix scroll to anchor
    
    * fix scroll to anchor

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Box } from 'grommet';
 import Nav from './Nav';
 
-const Page = ({ children, background }) => (
-  <Box pad="large" background={background}>
-    <Box>
-      <Nav />
-      <Box margin={{ top: 'large' }}>{children}</Box>
-    </Box>
-  </Box>
-);
+export default class Page extends Component {
+  componentDidMount() {
+    this.scrollToSection();
+  }
+
+  componentDidUpdate() {
+    this.scrollToSection();
+  }
+
+  scrollToSection = () => {
+    const name = window.location.hash.split('#')[1];
+    if (name) {
+      document.getElementById(name).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+
+  render() {
+    const { background, children } = this.props;
+    return (
+      <Box pad="large" background={background}>
+        <Box>
+          <Nav />
+          <Box margin={{ top: 'large' }}>{children}</Box>
+        </Box>
+      </Box>
+    );
+  }
+}
 
 Page.propTypes = Box.propTypes;
-
-export default Page;
